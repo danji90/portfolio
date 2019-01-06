ff5d17ed7ed9aad7f54faa4ccd5a54d6aca1412c
danji90 <danji_ma90@hotmail.com>
2019-01-06 21:50:42 +0100

    fixed scroll animation for skill bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,29 @@ jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
 
-    $(window).on('load', function() {
+    $('.level-bar-inner').each(function() {
+        var itemWidth = $(this).data('level');
+        // $(this).css('width', itemWidth)
+        $(this).animate({
+          width: itemWidth
+        }, 800);
+      })
 
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-
-        });
-
-    });
+    // $(window).on('load', function() {
+    //
+    //     $('.level-bar-inner').each(function() {
+    //
+    //         var itemWidth = $(this).data('level');
+    //
+    //         // $(this).animate({
+    //         //     width: itemWidth
+    //         // }, 800);
+    //
+    //         // $(this).css('width', itemWidth)
+    //
+    //     });
+    //
+    // });
 
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
